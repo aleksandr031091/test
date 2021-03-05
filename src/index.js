ec1af525cf6a8655c92d005b9b055037446a2d60
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-03-05 16:05:46 +0200

    in-commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import test from "./js/new";
+import { b, c } from "./js/name";
+import * as io from "./js/name";
+
+test();
+
+b();
+console.log(c);
+console.log(io);
---
+++ src/js/name.js
@@ -0,0 +1,9 @@
+const a = 5;
+
+const b = function () {
+  console.log("ok");
+};
+
+const c = [1, 2, 3];
+
+export { b, c };
